30148b59639daba4a49a3d18cf8e13f5c79ddfbd
dani-restu <danierestu4@gamil.com>
2019-10-24 11:15:13 +0700

    first commit

--- src/com/xsis/day1/Practice01.java
+++ src/com/xsis/day1/Practice01.java
@@ -2,7 +2,11 @@ package com.xsis.day1;
 
 public class Practice01 {
     public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
         System.out.println("hello Xsis Academy");
         System.out.println("hello /nAcademy");
+        System.out.println("Hello /tAcademy");
     }
 }
